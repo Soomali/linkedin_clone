c762327af2470ff483b0c70eb6f8e0b708906e2c
makaroniii <mehmetaliguunaydin@gmail.com>
2022-03-28 21:09:51 +0300

    added skeleton of the main page and the drawer

---
+++ lib/views/mainPage/app_bar.dart
@@ -0,0 +1,26 @@
+part of 'main_page.dart';
+
+class LinkedInAppBar extends StatelessWidget {
+  const LinkedInAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return SliverAppBar(
+      systemOverlayStyle: SystemUiOverlayStyle(
+        statusBarColor: Theme.of(context).primaryColor,
+        statusBarIconBrightness: Brightness.dark, //android için
+        statusBarBrightness: Brightness.light, //ios için
+      ),
+      floating: true,
+      leadingWidth: size.width * 0.12,
+      leading: const Padding(
+          padding: EdgeInsets.only(left: 8.0), child: CustomAvatar()),
+      actions: const [
+        Padding(padding: EdgeInsets.only(right: 8), child: Icon(Icons.message))
+      ],
+      title: const SearchBar(),
+    );
+  }
+}
---
+++ lib/views/mainPage/search_bar.dart
@@ -0,0 +1,27 @@
+part of 'main_page.dart';
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    return SizedBox(
+      height: height * 0.04,
+      child: TextField(
+        maxLines: 1,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.all(5),
+            filled: true,
+            fillColor: Colors.grey,
+            prefixIcon: Icon(
+              Icons.search,
+              color: Theme.of(context).iconTheme.color,
+            ),
+            hintText: 'Arama Yap',
+            hintStyle: TextStyle(color: Colors.white),
+            border: OutlineInputBorder()),
+      ),
+    );
+  }
+}
